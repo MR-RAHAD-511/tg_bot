8c278b158acf0fc21fb7adae19cd774c31eeb7c1
Bot Bots <36937402-ytprahad@users.noreply.replit.com>
2024-12-05 05:36:38 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ownerUsernames: ["rabbyhosainRahad"],//Your Username
+  ownerNumber: "+8801885115218",//Your Number
+  admin: ["5813798304"], // your id
+  prefix: "/", // dont change prefix 
+  telegramBotToken: "7207503618:AAFYcO6bclGaD5dXktauhLa815b4WPOlLfQ",//Your Bot Tokens
+  imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
+  port: process.env.PORT || 8053 || 8053, // port
+  mess: {
+    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    eror: "Internal Server Eror 😵",
+    owner: "Sorry, this command can only be accessed by the owner!",
+    group: "Sorry, this command can only be used within a group!",
+    wait: "Your request is being processed...",
+  },
+};
