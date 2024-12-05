6fe065c1766ada5ab322723a2992b7c20b9cefc2
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-12-05 12:52:09 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   ownerNumber: "+8801885115218",//Your Number
   admin: ["5813798304"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "7207503618:AAFYcO6bclGaD5dXktauhLa815b4WPOlLfQ",//Your Bot Tokens
+  telegramBotToken: "7207503618:AAEnwtfXo-LsdSI24BiU6s6C1K_YjI2ojno",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
